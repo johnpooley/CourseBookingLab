84ee37b07ca04a3baba039350d7706a1b324a66d
JohnPooley <thevinylvandal@hotmail.com>
2020-04-02 12:04:11 +0100

    add customer properties

--- src/main/java/com/codeclan/CourseBookingLab/models/Customer.java
+++ src/main/java/com/codeclan/CourseBookingLab/models/Customer.java
@@ -1,13 +1,22 @@
 package com.codeclan.CourseBookingLab.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 
 @Entity
 @Table(name = "customers")
 public class Customer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+    @Column
+    private String town;
+    @Column
+    private int age;
 
     public Customer(){}
 }
